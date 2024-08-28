0d36fbaa4ebd1cf8c9b2f364b7b3fe5c6862e959
diegofres <a223221580@unison.mx>
2024-08-28 00:05:39 -0700

    cmmit

--- src/main/java/ExprecionesRegulares/exprecionesRegulares.java
+++ src/main/java/ExprecionesRegulares/exprecionesRegulares.java
@@ -9,7 +9,7 @@ public class exprecionesRegulares {
 
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Por favor, proporciona una cadena como argumento.");
+            System.out.println("Escribe una cadena como argumento");
                 return;
             }
   String cadena = args[0];
@@ -17,14 +17,14 @@ public class exprecionesRegulares {
   Pattern p1 = Pattern.compile("\\bHola mundo\\b");
     Matcher m1 = p1.matcher(cadena);
         if (m1.matches()) {
-            System.out.println("La cadena coincide con la frase exacta 'Hola mundo'.");
+            System.out.println("La cadena coincide con la frase exacta Hola mundo");
             }
 
   // Buscar la frase Hola mundo en cualquier combinación de mayúsculas y minúsculas
     Pattern p2 = Pattern.compile("(?i)\\bHola mundo\\b");
           Matcher m2 = p2.matcher(cadena);
             if (m2.matches()) {
-                System.out.println("La cadena coincide con la frase 'Hola mundo' en cualquier combinación de mayúsculas y minúsculas.");
+                System.out.println("La cadena coincide con la frase Hola mundo en cualquier combinación de mayúsculas y minúsculas.");
 
 
       }
@@ -40,18 +40,18 @@ public class exprecionesRegulares {
      Pattern p4 = Pattern.compile("^[a-zA-Z0-9._%+-]+@(unison\\.mx|uson\\.mx)$");
         Matcher m4 = p4.matcher(cadena);
             if (m4.matches()) {
-                System.out.println("La cadena es un correo válido de la Universidad de Sonora.");
+                System.out.println("La cadena es un correo válido de la Universidad de Sonora");
    }
 
  // Validar el nombre de un archivo prefijo ISI, año y extensión específica
      Pattern p5 = Pattern.compile("^ISI\\d{4}-[12]\\.(txt|csv)$");
             Matcher m5 = p5.matcher(cadena);
             if (m5.matches()) {
-                System.out.println("La cadena es un nombre de archivo válido según el formato especificado.");
+                System.out.println("La cadena es un nombre de archivo válido ");
             }
 
             if (!m1.matches() && !m2.matches() && !m3.matches() && !m4.matches() && !m5.matches()) {
-                System.out.println("La cadena no coincide con ninguna de las expresiones regulares especificadas.");
+                System.out.println("La cadena no coincide con las expresiones regulares ");
             }}
     }
 
